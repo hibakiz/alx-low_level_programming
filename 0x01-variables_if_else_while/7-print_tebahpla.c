41bba32ee360100e7adc2dd1e17e3c0f799dc11b
hibakiz <hbosalah98sd@gmail.com>
2023-08-11 21:52:57 +0300

    z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print using puts'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+	putchar(i);
+
+	}
+	putchar('\n');
+	return (0);
+}
