4b061c16fcbc762ec9b5bc81003f687850a55261
hibakiz <hbosalah98sd@gmail.com>
2023-08-11 21:27:50 +0300

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print using puts'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+	putchar(i);
+	
+	}
+	putchar('\n');
+	return (0);
+}
