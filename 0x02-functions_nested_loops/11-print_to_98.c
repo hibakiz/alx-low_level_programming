d08ff99010768688417835a2308f229a396c0217
hibakiz <hbosalah98sd@gmail.com>
2023-08-16 01:29:53 +0300

    tt

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,6 +9,8 @@
 void print_to_98(int n)
 {
 	int i;
+	if (n < 98)
+	{
 	for (i = n; i <= 98; i++)
 	{
 	if (i == 98)
@@ -16,4 +18,13 @@ void print_to_98(int n)
 	else
 	printf("%d, ", i);
 }
+	else if (n > 98)
+	{
+	for (i = n; i >= 98; i--)
+	{
+	if (i == 98)
+	printf("%d\n", i);
+	else
+	printf("%d, ", i);
+	}
 }
