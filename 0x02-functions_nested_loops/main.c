01663c10511d59c405b0f1abba41a53e90176937
hibakiz <hbosalah98sd@gmail.com>
2023-08-16 00:01:11 +0300

    +

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,16 +1,18 @@
+#include <limits.h>
+#include "main.h"
+
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-#include "main.h"
-
 int main(void)
 {
 	int r;
-	r = print_sign(-23);
-	putchar(r + '0');
-	r = print_sign(23);
+
+	r = print_sign(98);
+	if (r < 0)
+		r *= -1;
 	putchar(r + '0');
 	putchar('\n');
 	return (0);
