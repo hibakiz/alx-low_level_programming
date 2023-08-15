0e217b591ab28ff7e206be3e4bd1ffa158050fc0
hibakiz <hbosalah98sd@gmail.com>
2023-08-15 23:14:20 +0300

    d

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * pritn_alphabet_x10 - print a-z 10 times.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+}
+}
