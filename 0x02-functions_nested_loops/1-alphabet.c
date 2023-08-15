afc63dbdb8b138dc9d6fb4677097994725b69913
hibakiz <hbosalah98sd@gmail.com>
2023-08-15 21:52:13 +0300

    wow

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'print using puts'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int putchar(int character);
+void print_alphabet(void);
