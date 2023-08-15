32735d5b0245ab5497e6021b85bd458ede2ea0c8
hibakiz <hbosalah98sd@gmail.com>
2023-08-15 23:30:10 +0300

    finally

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  _islower - print a-z 10 times.
+ * @c:  is the input
+ *
+ * Return:Always 0.
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	return (1);
+	else
+	return (0);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -7,6 +7,9 @@
 
 int main(void)
 {
-	print_alphabet_x10();
+	int r;
+	r = _islower('H');
+	putchar(r + '0');
+	putchar('\n');
 	return (0);
 }
