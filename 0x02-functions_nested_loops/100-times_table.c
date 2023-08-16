36778ad1e8d7367ea0c881c7a626a7c1b0a0e3bc
hibakiz <hbosalah98sd@gmail.com>
2023-08-16 05:02:25 +0300

    tt

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include<stdio.h>
 /**
- *  print_to_98 - test if a char.
+ *  print_times_table - test if a char.
  * @n:  is the input
  *
  * Return:Always 0.
  */
- void print_times_table(int n)
+void print_times_table(int n)
 {
 	int i, j;
-	
+
 	if (n > 15 || n < 0)
 	printf("wtf");
 	else
@@ -18,16 +18,18 @@
 		{
 			for (j = 0; j <= n ; j++)
 			{
-				if(j != n)
+				if (j != n)
 				{
+				if (j <= 9)
 				printf("%d,   ", i * j);
+				else
+				printf("%d", i * j);
 				}
 				else
 				{
 				printf("%d\n", i * j);
 			}
 			}
-		
-		}
-	} 
+
+		}	}
 }
