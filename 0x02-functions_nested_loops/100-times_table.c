dc308bcd10d364026612c611842bfd9426ccad02
hibakiz <hbosalah98sd@gmail.com>
2023-08-16 05:04:50 +0300

    tt

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,32 +4,47 @@
  *  print_times_table - test if a char.
  * @n:  is the input
  *
- * Return:Always 0.
+ * Return:Alwajs 0.
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i = 0, j, k;
 
 	if (n > 15 || n < 0)
-	printf("wtf");
-	else
+		return;
+	while (i <= n)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n ; j++)
+			k = i * j;
+			if (k > 99)
 			{
-				if (j != n)
-				{
-				if (j <= 9)
-				printf("%d,   ", i * j);
-				else
-				printf("%d", i * j);
-				}
-				else
-				{
-				printf("%d\n", i * j);
+				_putchar(k / 100 + '0');
+				_putchar((k / 10 % 10) + '0');
+				_putchar(k % 10 + '0');
 			}
+			else if (k > 9)
+			{
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
 			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+				_putchar(k + '0');
 
-		}	}
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		i++;
+	}
 }
