b2bce80ca4c5435205185985b25896a46fb0c79b
hibakiz <hbosalah98sd@gmail.com>
2023-08-18 01:21:33 +0300

    0x04

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'tsks'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		printf("Fizz ");
+		else if (i % 5 == 0)
+		printf("Buzz ");
+		else if (i % 5 == 0 && i % 3 == 0)
+		printf("FizzBuzz ");
+		else
+		printf("%d ", i);
+	}
+	return (0);
+}
