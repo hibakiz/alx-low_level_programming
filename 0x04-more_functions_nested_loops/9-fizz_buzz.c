999d875a1f1b1cbb549e6fe78d6ea5602b6ef6e7
hibakiz <hbosalah98sd@gmail.com>
2023-08-18 01:41:52 +0300

    0x04

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,14 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
-		printf("FizzBuzz ");
+		printf("FizzBuzz");
 		else if (i % 5 == 0)
-		printf("Buzz ");
+		printf("Buzz");
 		else if (i % 3 == 0)
-		printf("Fizz ");
+		printf("Fizz");
 		else
-		printf("%d ", i);
+		printf("%d", i);
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
