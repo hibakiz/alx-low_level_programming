2d881843b02a51216f769ce3863d2c4fea574e18
hibakiz <hbosalah98sd@gmail.com>
2023-08-18 00:26:29 +0300

    0x04

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * more_numbers- Entry point
+ *
+ * Description: 'tsks'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i, x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+}
