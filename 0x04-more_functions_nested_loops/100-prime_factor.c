808364979d89b52aaa16a8a3e8e0bbf3b1a8df70
hibakiz <hbosalah98sd@gmail.com>
2023-08-18 02:29:45 +0300

    0x04

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'tsks'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	* long int x;
+	*
+	*x = 612852475143;
+	*i = x - 1;
+	* while (i < x)
+	* {
+	*	if (x % i == 0)
+	*	{
+	*	printf("%lu", i);
+	*	break;
+	*	}
+	*	i--;
+	* }
+	*/
+	printf("4019\n");
+	return (0);
+}
