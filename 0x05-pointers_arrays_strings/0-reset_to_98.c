ae5a7cbd49a1012cc8cff17e0620957cf1a97bdf
hibakiz <hbosalah98sd@gmail.com>
2023-08-21 22:18:53 +0300

    0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ *@n: int
+ * Description: 'tsks'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This for 0x05 tasks
