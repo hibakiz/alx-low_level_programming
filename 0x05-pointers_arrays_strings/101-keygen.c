477588729ece807f927c29b5de872e3aa6176b36
hibakiz <hbosalah98sd@gmail.com>
2023-08-22 03:25:33 +0300

    0x05

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/*
+* main - tsks
+*
+* return 0
+*/
 int main(void)
 {
-	int myrand;
+	int x;
 	int count;
 	int total;
 
 	srand(time(NULL));
 	for (count = 0, total = 2772; total > 122; count++)
 	{
-		myrand = (rand() % 125) + 1;
-		printf("%c", myrand);
-		total -= myrand;
+		x = (rand() % 125) + 1;
+		printf("%c", x);
+		total -= x;
 	}
 	printf("%c", total);
 
