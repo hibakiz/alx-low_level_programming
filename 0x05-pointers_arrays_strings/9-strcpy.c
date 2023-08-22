5ff51f9a27ae35e1c38daa78ea877869a758adcd
hibakiz <hbosalah98sd@gmail.com>
2023-08-22 03:04:37 +0300

    0x05

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int j, i = 0;
 
 	while (*(src + i) != '\0')
 	{
-	*(dest + i) = *(src + i);
-	i++;
+		i++;
 	}
+	for (j = 0; j < i; j++)
+		*(dest + i) = *(src + i);
+	dest[i] = '\0';
+
 	return (dest);
 }
