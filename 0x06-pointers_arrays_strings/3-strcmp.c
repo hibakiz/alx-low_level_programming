0a493e0c0e3132c2991d5ff0bbad03a594ef6164
hibakiz <hbosalah98sd@gmail.com>
2023-08-24 01:27:41 +0300

    0x06

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcmp - Entry point
+ *
+ * Description: 'tsks'
+ * @s1: char
+ * @s2: char
+ *
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			j = 0;
+		else if (s1[i] > s2[i])
+			j = -15;
+		else if (s1[i] < s2[i])
+			j = 15;
+	i++;
+	}
+	return (j);
+}
