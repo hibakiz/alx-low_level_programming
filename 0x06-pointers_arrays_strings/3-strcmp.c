1123f8ca5d7eb0ea31233383be9ab272358f1ffa
hibakiz <hbosalah98sd@gmail.com>
2023-08-24 01:43:18 +0300

    0x06

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,10 +16,8 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] == s2[i])
 			j = 0;
-		else if (s1[i] > s2[i])
-			j = -15;
-		else if (s1[i] < s2[i])
-			j = 15;
+		else
+			return (s1[i] - s2[i]);
 	i++;
 	}
 	return (j);
