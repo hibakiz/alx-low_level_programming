b2a43ab5038a13a6f89f9cc3e2131b9b7acaebc5
hibakiz <hbosalah98sd@gmail.com>
2023-08-24 01:04:41 +0300

    0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j = 0;
-	
+
 	while (src[j] != '\0')
 	{
 		j++;
@@ -19,9 +19,9 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i > j)
-			dest [i] = 0;
+			dest[i] = 0;
 		else
-			dest [i] = src[i];
+			dest[i] = src[i];
 	}
 	return (dest);
 }
