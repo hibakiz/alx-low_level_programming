4b2d6ccf26032d35960046b7116b5cd03d190342
hibakiz <hbosalah98sd@gmail.com>
2023-08-24 23:43:58 +0300

    0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,8 +18,10 @@ char *cap_string(char *str)
 		for (j = 0; sp[j] != '\0'; j++)
 		{
 			if (sp[j] == str[i])
+			{
 				if (str[i + 1] >= 97 && str[i + 1] <= 122)
 					str[i + 1] -= 32;
+		}
 		}
 	}
 	return (str);
