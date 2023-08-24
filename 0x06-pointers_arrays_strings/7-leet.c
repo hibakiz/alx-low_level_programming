cc9adc6b7b40304196308e53d14ddfbb6b3152c6
hibakiz <hbosalah98sd@gmail.com>
2023-08-25 00:23:33 +0300

    0x06

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,20 +10,45 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char let[] = "aAeEoOtTlL";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; let[j] != '\0'; j++)
+		{
+			if (j == 0 || j == 1)
+			{
+				if (str[i] == let[j])
+					str[i] = '4';
+			}
+
+			if (j == 2 || j == 3)
+			{
+				if (str[i] == let[j])
+					str[i] = '3';
+			}
+
+			if (j == 4 || j == 5)
+			{
+				if (str[i] == let[j])
+					str[i] = '0';
+			}
+
+			if (j == 6 || j == 7)
+			{
+				if (str[i] == let[j])
+					str[i] = '7';
+			}
+
+			if (j == 8 || j == 9)
+			{
+				if (str[i] == let[j])
+					str[i] = '1';
+			}
+			
+		}
+
 	}
 	return (str);
 
