3e39cbdb3fdad90b77f5cd53bbdfcb12697d8cce
hibakiz <hbosalah98sd@gmail.com>
2023-08-24 23:40:36 +0300

    0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,16 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int i, j;
+	char sp[] = "\t\n,;.!?\"(){} ";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == '!' || str[i] == '\t' || str[i] == ' ' ||str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '?' || str[i] == 34 || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		for (j = 0; sp[j] != '\0'; j++)
 		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-			str[i + 1] -= 32;
+			if (sp[j] == str[i])
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+					str[i + 1] -= 32;
 		}
 	}
 	return (str);
