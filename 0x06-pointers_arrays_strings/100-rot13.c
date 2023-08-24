6ff187ad0e3758ce40d4315846fc55239ed04399
hibakiz <hbosalah98sd@gmail.com>
2023-08-25 00:48:19 +0300

    0x06

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * rot13 - Entry point
+ *
+ * Description: 'tsks'
+ * @str: zft
+ *
+ * Return: Always 0 (Success)
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char let[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char val[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; let[j] != '\0'; j++)
+		{
+			if (let[j] == str[i])
+				str[i] = val[j];
+
+	}
+	}
+	return (str);
+
+}
