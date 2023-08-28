c941e82c0a95ddfd9063f71d334d432b6b372d60
hibakiz <hbosalah98sd@gmail.com>
2023-08-29 00:54:38 +0300

    0x07

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 1; s[i] != '\0'; i++)
 	{
 		if (s[0] == c)
 			return (s);
 		 else if (s[i] == c)
-			return (s + i);
-				
+			return (s + i);	
 	}
 	return (s + i);
 }
