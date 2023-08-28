408fd9c4fe21b131e16bf7761a57e34c3e86d7eb
hibakiz <hbosalah98sd@gmail.com>
2023-08-29 02:24:51 +0300

    0x07

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - Entry point
+ *
+ * Description: 'tsks'
+ * @to: char
+ * @s: int
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
