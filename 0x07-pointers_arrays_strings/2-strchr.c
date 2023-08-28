18bada1aa34ded21e0a8bdd227bbf1f06e7195de
hibakiz <hbosalah98sd@gmail.com>
2023-08-29 00:19:41 +0300

    0x07

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ *
+ * Description: 'tsks'
+ * @c: char
+ * @s: char
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			s += i;
+	}
+	return (s);
+}
