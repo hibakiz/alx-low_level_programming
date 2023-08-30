b095f205080f229ca62db9cf504b7035490e826b
hibakiz <hbosalah98sd@gmail.com>
2023-08-30 20:40:52 +0300

    0x08

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _print_rev_recursion - Entry point
+ * Description: 'tsks'
+ * @s: char
+ * Return: Always 0 (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+	else
+		return;
+
+
+}
