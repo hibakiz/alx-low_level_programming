2d45c9e7e0cffe45078b3d9aaba5a6b870ba588b
hibakiz <hbosalah98sd@gmail.com>
2023-08-30 20:08:09 +0300

    0x08

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - Entry point
+ *
+ * Description: 'tsks'
+ * @s: char
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	_putchar('\n');
+}
