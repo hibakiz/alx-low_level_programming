2c024901dfc1ff64a1aa3b3f71c907127723807e
hibakiz <hbosalah98sd@gmail.com>
2023-09-01 23:35:10 +0300

    0x0AAA

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * @argc: int
+ * Description: 'print using puts'
+ * @argv: int
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
