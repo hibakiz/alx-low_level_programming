0a3a22a843e5c43fc2b2b21e478a83263fb3053f
hibakiz <hbosalah98sd@gmail.com>
2023-09-01 23:45:07 +0300

    0x0AAA

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * @argv: int
  * Return: Always 0 (Success)
  */
-int main(int  argc, char __attribute__((unused)) *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
