b651e4b712e86665d04b43b35029072625e14a5a
hibakiz <hbosalah98sd@gmail.com>
2023-09-05 17:13:21 +0300

    0x0BB Dynamic

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+* free_grid - check the code
+* @width: int
+* @height: int
+* Return: Always 0.
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
