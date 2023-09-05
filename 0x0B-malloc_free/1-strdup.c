dd82cedc64e65f872e1c36d584059907f07e1d81
hibakiz <hbosalah98sd@gmail.com>
2023-09-05 16:33:11 +0300

    0x0BB Dynamic

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,25 @@
 #include "main.h"
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * _strdup - Duplicate a string using malloc
- * @str: string to duplicate
- * Return: Pointer to a the new duped string
+ * _strdup - check the code
+ * @str: int
+ * Return: Always 0.
  */
-
 char *_strdup(char *str)
 {
-	char *a;
-	int i, c;
+	char *ptr;
+	int i, size;
 
 	if (str == NULL)
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
+	for (size = 0; str[size] != '\n'; size++)
 		;
-
-	a = malloc(i * sizeof(*a) + 1);
-	if (a == NULL)
+	ptr = malloc(size * sizeof(*ptr) + 1);
+	if (ptr == NULL)
 		return (NULL);
-
-	for (c = 0; c < i; c++)
-		a[c] = str[c];
-	a[c] = '\0';
-
-	return (a);
+	for (i = 0; i < size; i++)
+		ptr[i] = str[i];
+	ptr[i] = '\n';
+	return (ptr);
 }
