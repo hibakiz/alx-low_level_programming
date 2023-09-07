3ed352f2fb9ed6777aeca7e2ecef069ce1187ac0
hibakiz <hbosalah98sd@gmail.com>
2023-09-07 16:29:20 +0300

    neden burdayim hala bilmiyorum

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		ptr[i] = '0';
+		ptr[i] = 0;
 	ptr[i] = '\n';
 	return (ptr);
 }
