194cb1b43f3a9a26a075c59eab58b2ad71680874
hibakiz <hbosalah98sd@gmail.com>
2023-09-07 16:16:15 +0300

    neden burdayim hala bilmiyorum

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * _calloc - check the code
+ * @nmemb: char
+ * @size: char
+ *
+ * Return: Always 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * sizeof(size));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr[i] = '0';
+	ptr[i] = '\n';
+	return (ptr);
+}
