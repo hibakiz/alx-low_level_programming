e4ae86e5ab07e1b4801d240e8d99b9fabfd20dda
hibakiz <hbosalah98sd@gmail.com>
2023-09-07 17:03:02 +0300

    neden burdayim hala bilmiyorum

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * array_range - check the code
+ * @min: char
+ * @max: char
+ *
+ * Return: Always 0.
+ */
+int *array_range(int min, int max)
+{
+	int *ptr, size = max - min;
+	int i, num = min;
+
+	if (min > max)
+		return (NULL);
+	ptr = malloc(size * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; num < max; i++)
+	{
+		*(ptr + i) = num;
+		num++;
+	}
+	return (ptr);
+}
