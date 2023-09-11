2254fb47d2c4721472e1b68c45323f2ed350f924
hibakiz <hbosalah98sd@gmail.com>
2023-09-11 16:44:47 +0300

    neden burdayim hala bilmiyorum extra extraaaaaaa

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			d->name = "(nil)";
 		if (d->owner == NULL)
-			d->name = "(nil)";
+			d->owner = "(nil)";
 		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 	else
