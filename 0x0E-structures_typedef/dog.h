76ea19e30a3d8bc8bb05f742d908bab2f9955024
hibakiz <hbosalah98sd@gmail.com>
2023-09-11 16:49:13 +0300

    neden burdayim hala bilmiyorum extra extraaaaaaa

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+ 
 /**
  * struct dog - tsts
  * @name: int
@@ -8,12 +9,12 @@
  *
  * Description: dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
