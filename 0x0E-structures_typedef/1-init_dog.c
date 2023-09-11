2eeeba923aab5cf8114153b4ed07b0eaa5293e76
hibakiz <hbosalah98sd@gmail.com>
2023-09-11 16:02:38 +0300

    neden burdayim hala bilmiyorum extra extraaaaaaa

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - check the code
+ * @d: int
+ * @name: int
+ * @age: int
+ * @owner: int
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
