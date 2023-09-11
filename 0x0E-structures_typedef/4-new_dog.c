51a8d8e18b32fd27a11133a196299251693f9aea
hibakiz <hbosalah98sd@gmail.com>
2023-09-11 17:09:17 +0300

    neden burdayim hala bilmiyorum extra extraaaaaaa

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,6 +25,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	strcpy(namecpy, name);
 	strcpy(ownercpy, owner);
 	new = malloc(sizeof(dog_t));
+	if (new = NULL)
+		return (NULL);
 	new->name = name;
 	new->age = age;
 	new->owner = owner;
