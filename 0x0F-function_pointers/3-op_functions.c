62bfffe2a47fdbe737c6ef011729a41fdc86a54e
hibakiz <hbosalah98sd@gmail.com>
2023-09-13 21:13:42 +0300

    umutum var

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - fun name
+ * @a: var
+ * @b: var
+ * Return: sucess.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - fun name
+ * @a: var
+ * @b: var
+ * Return: sucess.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - fun name
+ * @a: var
+ * @b: var
+ * Return: sucess.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - fun name
+* @a: var
+* @b: var
+* Return: sucess.
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - fun name
+* @a: var
+* @b: var
+* Return: sucess.
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
