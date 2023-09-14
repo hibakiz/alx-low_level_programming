9e9d61ec29fdf3cba1ceae8b8806559bb60a32cc
hibakiz <hbosalah98sd@gmail.com>
2023-09-14 16:46:52 +0300

    looks ok now

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - fun name
+* @n: var
+*
+* Return: success.
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list ptr;
+
+	if (n == 0)
+		return (0);
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ptr, int);
+	va_end(ptr);
+
+	return  (sum);
+
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+sanirun bu iyi 
