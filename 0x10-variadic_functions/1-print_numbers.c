87321559a4c112b523cb559db03baa927f7012a5
hibakiz <hbosalah98sd@gmail.com>
2023-09-14 17:18:54 +0300

    looks ok now

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers - fun name
+* @n: var
+* @separator: var
+* Return: success.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int j;
+	va_list ptr;
+
+	va_start(ptr, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d", va_arg(ptr, unsigned int));
+		j = 0;
+		{
+			while (separator[j])
+			{
+				printf("%c", separator[j]);
+				j++;
+			}
+		}
+	}
+	printf("%d\n", va_arg(ptr, unsigned int));
+	va_end(ptr);
+
+}
