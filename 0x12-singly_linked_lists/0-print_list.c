01b12e544da1f3a4575245d86af21b5bcfc5d28e
hibakiz <hbosalah98sd@gmail.com>
2023-09-21 22:30:31 +0300

    new topics for the 1st time

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,19 +4,20 @@
  * @h : var
  * Return: Always 0.
  */
+ 
 size_t print_list(const list_t *h)
 {
 	size_t el;
 
 	el = 0;
-		while (h != NULL)
-		{
-			el++;
-			if (h->str == NULL)
-				printf("[%d] %s\n", 0, "(nil)");
-			else
-				printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
+	while (h != NULL)
+	{
+		el++;
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
 	return (el);
 }
