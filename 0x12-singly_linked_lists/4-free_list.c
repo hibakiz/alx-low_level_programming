16b4f760c73ef98d165ed4de1e005a11d756fbb3
hibakiz <hbosalah98sd@gmail.com>
2023-09-22 00:35:22 +0300

    new topics for the 1st time

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_list - prints all the elements of a list_t list.
+ * @head: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	free(head->str);
+	free(head);
+}
