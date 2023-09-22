b485432909771a290899f33e489ef7c7999dfc10
hibakiz <hbosalah98sd@gmail.com>
2023-09-22 21:58:38 +0300

    advance

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * before_main - prints all the elements of a list_t list.
+ * Return: number of elements in the list.
+ */
+
+void before_main(void)
+{
+	printf("You're beat!\n");
+	printf("and yet, you must allow,\nI bore my house upon my back!\n");
+}
