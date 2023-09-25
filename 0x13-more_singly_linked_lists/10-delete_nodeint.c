88c82972a715d849ceb57cf4b08adfb1aedcdc76
hibakiz <hbosalah98sd@gmail.com>
2023-09-25 23:19:47 +0300

    more and more linked

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - prints all the elements of a list_t list.
+ * @head: singly linked list.
+ * @index: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int n = 0;
+	listint_t *cur, *prev;
+
+	cur = *head;
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(cur);
+		return (1);
+	}
+	while (cur)
+	{
+		if (cur == NULL || cur->next == NULL)
+			return (-1);
+		if (n == index)
+		{
+			prev->next = cur->next;
+			free(cur);
+			return (1);
+		}
+		prev = cur;
+		cur = cur->next;
+		n++;
+	}
+	return (-1);
+}
