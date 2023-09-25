a29f0c50a3d85f12927ec579b9589aafcde887b5
hibakiz <hbosalah98sd@gmail.com>
2023-09-25 23:22:37 +0300

    more and more linked

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *cur, *prev;
 
 	cur = *head;
-	if (*head == NULL)
+	if (cur == NULL)
 		return (-1);
 	if (index == 0)
 	{
@@ -22,8 +22,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (cur)
 	{
-		if (cur == NULL || cur->next == NULL)
-			return (-1);
 		if (n == index)
 		{
 			prev->next = cur->next;
