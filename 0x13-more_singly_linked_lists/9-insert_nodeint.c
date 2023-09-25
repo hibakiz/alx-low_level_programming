aa2aa2629f4c53dda6801af1cf100b9b8dc0a871
hibakiz <hbosalah98sd@gmail.com>
2023-09-25 22:33:03 +0300

    more and more linked

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - prints all the elements of a list_t list.
+ * @head: singly linked list.
+ * @idx: singly linked list.
+ * @n: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int index = 0;
+	listint_t *new, *cur;
+
+	cur = *head;
+	if (head == NULL)
+		return (0);
+	while (cur)
+	{
+		if (index == idx)
+		{
+			new = malloc(sizeof(listint_t));
+			if (new == NULL)
+				return (NULL);
+			new->n = n;
+			*head = cur;
+			return (new);
+		}
+		else
+		*head = (*head)->next;
+		index++;
+	}
+	*head = cur;
+	return (NULL);
+}
