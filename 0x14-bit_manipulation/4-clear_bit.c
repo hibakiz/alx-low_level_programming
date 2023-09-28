d556e710419760c9f16f036fb3f37a234db1416c
hibakiz <hbosalah98sd@gmail.com>
2023-09-28 18:16:01 +0300

    oooooooooooooh let's play

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - Function name
+ * @n: var
+ * @index : var
+ * Return: Always 0.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > 31 || n == NULL)
+		return (-1);
+
+	*n &= ~(1 << index);
+	return (1);
+}
