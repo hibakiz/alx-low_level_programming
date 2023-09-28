4748c71078fbada5420d6d4a8fdd157ef373f1d6
hibakiz <hbosalah98sd@gmail.com>
2023-09-28 17:48:32 +0300

    oooooooooooooh let's play

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,8 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
-
-	bit = (n >> index) & 1;
-	if ((bit != 0 && bit != 1))
+		bit = (n >> index) & 1;
+		if ((bit == 0 || bit == 1))
+			return (bit);
 		return (-1);
-	return (bit);
 }
