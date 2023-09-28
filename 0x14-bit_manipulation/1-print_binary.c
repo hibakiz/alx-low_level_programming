6dbf246ffc3ede0d6fab39350d9d934d250fe41b
hibakiz <hbosalah98sd@gmail.com>
2023-09-28 17:34:03 +0300

    oooooooooooooh let's play

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,13 +7,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, len;
-	unsigned long int bit;
-
-	len = sizeof(n) * 8;
-	for (i = len - 1; i >= 0; i--)
-	{
-		bit = (n >> i) & 1;
-		_putchar(bit + '0');
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar('0' + (n & 1));
 }
