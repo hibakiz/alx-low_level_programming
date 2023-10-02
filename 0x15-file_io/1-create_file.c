df7e0cfc05f56aed878c667bfd40d18e86a66256
hibakiz <hbosalah98sd@gmail.com>
2023-10-03 02:58:52 +0300

    baaaaaaaaaaaad

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 		if (fd == -1)
 			return (-1);
 
-	write_ = write(fd, text_content, sizeof(text_content) - 1);
+	write_ = write(fd, text_content, strlen(text_content));
 	if (write_ == -1)
 	{
 		close(fd);
