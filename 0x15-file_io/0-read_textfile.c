1e2f0a2182e837fa9d89911236d95c1f28aec58e
hibakiz <hbosalah98sd@gmail.com>
2023-10-03 02:01:39 +0300

    baaaaaaaaaaaad

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - Function name
+ * @filename : var
+ * @letters : var
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fd;
+	ssize_t number_of_letters;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = fopen(filename, "r");
+	if (fd == NULL)
+		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+	number_of_letters = fread(buffer, 1, letters, fd);
+	printf("%s", buffer);
+
+	fclose(fd);
+
+	return (number_of_letters);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
