0128425fcd8bdb899318e6d1835427799a1cb0e6
hibakiz <hbosalah98sd@gmail.com>
2023-11-09 22:21:12 +0300

    why C again and again bitmyan

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* print_dlistint - print the list
+* @h: DLL
+* Return: new
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
