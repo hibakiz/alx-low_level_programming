da8a99db51f70ee55f82a38fbf64e511fa245714
hibakiz <hbosalah98sd@gmail.com>
2023-11-10 00:59:56 +0300

    why C again and again bitmyan

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,13 +10,16 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i;
 	dlistint_t *cur;
 
-	cur = head;
-	for (i = 0; i <= index; i++)
+	if (head)
 	{
-		if (i != index)
-			cur = cur->next;
-		else
-			return (cur);
+		cur = head;
+		for (i = 0; i <= index; i++)
+		{
+			if (i != index)
+				cur = cur->next;
+			else
+				return (cur);
+		}
 	}
 	return (NULL);
 }
