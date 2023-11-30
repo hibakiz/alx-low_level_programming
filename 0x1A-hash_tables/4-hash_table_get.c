8524ef20ebfcb8e073fa84c7f076181d863dfffd
hibakiz <hbosalah98sd@gmail.com>
2023-12-01 02:52:19 +0300

    I do want to leave alx

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the index of a key
+ * @ht: the hash table
+ * @key: the key to add
+ * Return: the value with the key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+	unsigned long int index;
+
+	if (!ht || !ht->array || !ht->size ||
+	    !key || strlen(key) == 0)
+		return (0);
+
+
+	index = key_index((const unsigned char *)key, ht->size);
+		if (ht->array[index])
+			return (ht->array[index]->value);
+
+	return (NULL);
+}
