b58ef4d724f09331b9dc62118d4e0d5822485a28
hibakiz <hbosalah98sd@gmail.com>
2023-12-01 02:53:23 +0300

    I do want to leave alx

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (!ht || !ht->array || !ht->size ||
 	    !key || strlen(key) == 0)
-		return (0);
+		return (NULL);
 
 
 	index = key_index((const unsigned char *)key, ht->size);
