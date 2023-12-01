762867d5ec3e8bae33f5b6c7609e5aef3c3cd23f
hibakiz <hbosalah98sd@gmail.com>
2023-12-01 11:51:52 +0300

    I do want to leave alx

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * shash_table_create - creates a sorted table
- * @size: size of the table
+ * shash_table_create - creates a sorted hash table
+ * @size: size of the hash table
  *
- * Return: pointer to the new table
+ * Return: pointer to the new table, or NULL on failure
  */
 shash_table_t *shash_table_create(unsigned long int size)
 {
@@ -31,11 +31,11 @@ shash_table_t *shash_table_create(unsigned long int size)
 }
 
 /**
- * make_shash_node - create a node
+ * make_shash_node - makes a node for the sorted hash table
  * @key: key for the data
  * @value: data to be stored
  *
- * Return: pointer to the new node
+ * Return: pointer to the new node, or NULL on failure
  */
 shash_node_t *make_shash_node(const char *key, const char *value)
 {
@@ -62,7 +62,7 @@ shash_node_t *make_shash_node(const char *key, const char *value)
 }
 
 /**
- * add_to_sorted_list - add a node
+ * add_to_sorted_list - add a node to the sorted (by key's ASCII) linked list
  * @table: the sorted hash table
  * @node: the node to add
  *
@@ -99,12 +99,12 @@ void add_to_sorted_list(shash_table_t *table, shash_node_t *node)
 }
 
 /**
- * shash_table_set - sets a key to a value
+ * shash_table_set - sets a key to a value in the hash table
  * @ht: sorted hash table
  * @key: key to the data
  * @value: data to add
  *
- * Return: 1 on success
+ * Return: 1 on success, 0 otherwise
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
@@ -140,11 +140,11 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 }
 
 /**
- * shash_table_get - retrieve a value
+ * shash_table_get - retrieve a value from the hash table
  * @ht: hash table
  * @key: key to the data
  *
- * Return: the value associated with key
+ * Return: the value associated with key, or NULL on failure
  */
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
@@ -166,7 +166,7 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 }
 
 /**
- * shash_table_print - prints the sorted hash table
+ * shash_table_print - prints a sorted hash table
  * @ht: hash table to print
  *
  * Return: void
@@ -192,7 +192,7 @@ void shash_table_print(const shash_table_t *ht)
 }
 
 /**
- * shash_table_print_rev - prints the sorted hash table in reverse
+ * shash_table_print_rev - prints a sorted hash table in reverse
  * @ht: hash table to print
  *
  * Return: void
