fbed1fc8acfdb9b093ff11ed52a562a9ac601b8f
hibakiz <hbosalah98sd@gmail.com>
2024-01-24 22:51:22 +0300

    task3

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,7 +3,7 @@
 
 
 def island_perimeter(grid):
-    """that returns the perimeter of the 
+    """that returns the perimeter of the
     island described in grid:
 
 grid is a list of list of integers:
@@ -11,12 +11,12 @@ grid is a list of list of integers:
 1 represents a land zone
 One cell is a square with side length 1
 Grid cells are connected horizontally/vertically (not diagonally).
-Grid is rectangular, 
+Grid is rectangular,
 width and height don’t exceed 100
-Grid is completely surrounded by water, 
+Grid is completely surrounded by water,
 and there is one island (or nothing).
-The island doesn’t have “lakes” 
-water inside that isn’t connected to the 
+The island doesn’t have “lakes”
+water inside that isn’t connected to the
 water around the island)."""
     width = len(grid[0])
     height = len(grid)
