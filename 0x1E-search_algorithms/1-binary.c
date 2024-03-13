6c5be0d43ee3682a893327f8915c2cfb9e54951c
hibakiz <hbosalah98sd@gmail.com>
2024-03-14 02:06:11 +0300

    task 1 tany

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t low = 0, high = size - 1, mid = size;
 
 
-	while(low < high)
+	while(low <= high)
 	{
 		printf("Searching in array: ");
 		for (i = low; i < high; i++)
