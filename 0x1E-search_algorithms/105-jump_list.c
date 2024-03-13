2db027937b262eebdb777a102648deb2b985ea2f
hibakiz <hbosalah98sd@gmail.com>
2024-03-14 02:49:27 +0300

    task 105

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,49 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - function that searches for
+ * using the Linear search algorithm
+ * @list: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value:  the value to search for
+ * Return: the first index where value is located or NULL
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t index, k, m;
+	listint_t *prev;
+
+	if (list == NULL || size == 0)
+		return (NULL);
+
+	m = (size_t)sqrt((double)size);
+	index = 0;
+	k = 0;
+
+	do {
+		prev = list;
+		k++;
+		index = k * m;
+
+		while (list->next && list->index < index)
+			list = list->next;
+
+		if (list->next == NULL && index != list->index)
+			index = list->index;
+
+		printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
+
+	} while (index < size && list->next && list->n < value);
+
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)prev->index, (int)list->index);
+
+	for (; prev && prev->index <= list->index; prev = prev->next)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+	}
+
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,5 +13,6 @@ int exponential_search(int *array, size_t size, int value);
 int _binary_search(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
 int rec_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
